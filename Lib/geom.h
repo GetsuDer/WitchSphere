5cafe8e07ad2b88456b8b6c8e7e1e817d9a99fe1
Svetlana <svetlo_nika@mail.ru>
2021-03-26 13:14:26 +0300

    Learn how to create image with stbi_write

---
+++ Lib/geom.h
@@ -0,0 +1,7 @@
+struct Color {
+    float r;
+    float g;
+    float b;
+    float a;
+    Color(float _r = 0, float _g = 0, float _b = 0, float _a = 0) : r(_r), g(_g), b(_b), a(_a) {}
+};
--- Source/main.cpp
+++ Source/main.cpp
@@ -1,11 +1,36 @@
 #include <cstring>
 #include <iostream>
+#include <vector>
+#include <cmath>
+#include <algorithm>
 
+#include "geom.h"
+
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
 void 
 render(int size) {
     if (size < 0) {
         std::cerr << "error: negative picture size" << std::endl;
+        return;
     }
+    std::vector<Color> buffer(size * size);
+    for (int x = 0; x < size; x++) {
+        for (int y = 0; y < size; y++) {
+            buffer[x + y * size] = Color(x / float(size), y / float(size), 0.f, 1.f);
+        }
+    }
+
+    unsigned char *data = new unsigned char[size * size * 3];
+    for (int x = 0; x < size; x++) {
+        for (int y = 0; y < size; y++) {
+            data[0 + x * 3 + y * size * 3] = (unsigned char)(255 * std::max(0.f, std::min(1.f, buffer[x + y * size].r)));
+            data[1 + x * 3 + y * size * 3] = (unsigned char)(255 * std::max(0.f, std::min(1.f, buffer[x + y * size].g)));
+            data[2 + x * 3 + y * size * 3] = (unsigned char)(255 * std::max(0.f, std::min(1.f, buffer[x + y * size].b)));
+        }
+    }
+
+    stbi_write_png("328_derevyanko_v4v5", size, size, 3, data, size * 3);
     return;
 }
 
